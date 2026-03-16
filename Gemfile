06b5742f2c0bb5e0277b1512817d83bd65c1d06b
Andrew Lilley <terrenuvian_trilobite@tuta.io>
2026-03-16 08:57:13 -0700

    Preliminary Pre-formatting changes for base application

--- Gemfile
+++ Gemfile
@@ -57,4 +57,8 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  
+  gem 'bootstrap'
+  gem 'sassc-rails'
+  
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require "sassc-rails"
+require "bootstrap"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
